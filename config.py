c17cee16190b33fa5441e42f6891a3e2d9b0f33a
⏤͟͞ 𝙎𝙋𝘼𝙍𝙎𝙃 𝘽𝘼𝙉𝙄𝙔𝘼 <sparshshivhare2007@gmail.com>
2025-11-30 21:53:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,9 +6,4 @@ load_dotenv()
 API_ID = int(os.getenv("API_ID"))
 API_HASH = os.getenv("API_HASH")
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-MONGO_URI = os.getenv("MONGO_URI")
-
-# NEW
-ASSISTANT_SESSION = os.getenv("ASSISTANT_SESSION")   # Music ke liye (user session)
-OPENAI_KEY = os.getenv("OPENAI_KEY")                # Chat ke liye
-DOWNLOADS_DIR = os.getenv("DOWNLOADS_DIR", "downloads")
+SESSION = os.getenv("SESSION")  # assistant client ka string session
